6d1f6a0c4f6f68c3d1f88988ac78f9a7f3128eb1
Florent Clairambault <florent@habx.fr>
2020-05-31 02:30:52 +0200

    Giving up on SVGs

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 docker build -t im .
 docker run --rm im -list format
 docker run --rm im -list delegate
-docker run --rm -v $(pwd):/imgs im /imgs/test.svg /imgs/test-svg.png
+docker run --rm -v $(pwd):/imgs im -verbose -debug all /imgs/test.svg /imgs/test-svg.png
 docker run --rm -v $(pwd):/imgs -ti --entrypoint=/bin/bash im 
